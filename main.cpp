5799cac0bd49e3f7427dc502d2f271405ee329ce
Francesco Menzani <francesco@menzani.eu>
2020-12-25 20:06:29 +0100

    Use Qt Creator instead of Visual Studio

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <windows.h>
-#include <string>
 
-int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+int main()
 {
     PROCESS_INFORMATION ProcessInfo;
-    STARTUPINFO StartupInfo;
+    STARTUPINFOW StartupInfo;
     ZeroMemory(&StartupInfo, sizeof(StartupInfo));
     StartupInfo.cb = sizeof StartupInfo;
 
     wchar_t lpCommandLine[] = L"javaw -cp Jdoc.jar;lib/*;C:/Libs/* eu.menzani.jdoc.App";
     LPCWSTR lpCurrentDirectory = L"C:\\Apps\\Jdoc";
 
-    if (CreateProcess(NULL, lpCommandLine, NULL, NULL, FALSE, 0, NULL, lpCurrentDirectory, &StartupInfo, &ProcessInfo))
+    if (CreateProcessW(NULL, lpCommandLine, NULL, NULL, FALSE, 0, NULL, lpCurrentDirectory, &StartupInfo, &ProcessInfo))
     {
         CloseHandle(ProcessInfo.hThread);
         CloseHandle(ProcessInfo.hProcess);
